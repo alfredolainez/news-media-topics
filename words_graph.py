5c946d6a7b177a8519b88f44142be30629f6edd8
emalfredolainez@bizeulabs.com/em <alainez@stanford.edu>
2014-11-10 19:22:44 -0800

    Added token stemming

--- words_graph.py
+++ words_graph.py
@@ -1,7 +1,19 @@
 import networkx as network
 import nltk
+from nltk.stem.porter import PorterStemmer
 
-class GraphBuilder:
+def stem_words(tokens, language='english'):
+    """
+    Stems words in a list of tokens
+    """
+    stemmer = PorterStemmer()
+    stemmed_words = []
+    for token in tokens:
+        stemmed_words.append(stemmer.stem(token))
+
+    return stemmed_words
+
+class GraphBuilder(object):
     """
     Generic class for graph builders. It serves as a base class with a basic framework
     for text capabilities for building graphs.
@@ -11,8 +23,9 @@ class GraphBuilder:
         self.texts: raw unicode texts loaded
         self.text_sentences: for each text, list of tokenized and cleaned sentences
     """
-    def __init__(self, text_cleaner=None):
+    def __init__(self, text_cleaner=None, stem_words=True):
         self.text_cleaner = text_cleaner
+        self.stem_words = stem_words
         self.texts = []
         self.text_sentences = []
 
@@ -28,6 +41,9 @@ class GraphBuilder:
                 tokens = nltk.word_tokenize(sentence)
                 if self.text_cleaner is not None:
                     tokens = self.text_cleaner(tokens)
+                    if self.stem_words:
+                        tokens = stem_words(tokens)
+                    
                 tokens_sentences.append(tokens)
             self.text_sentences.append(tokens_sentences)
 
@@ -49,7 +65,8 @@ class SimpleGraphBuilder(GraphBuilder):
     by an edge with weight the number of times they appear in the same document
     """
 
-    def __init__(self, text_cleaner=None):
+    def __init__(self, text_cleaner=None, stem_words=True):
+        super(SimpleGraphBuilder, self).__init__(text_cleaner, stem_words)
         self.text_cleaner = text_cleaner
 
     def create_graph(self):
